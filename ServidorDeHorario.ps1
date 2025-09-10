e60c0eb716661a84afbf4bdc1b9cda93c3d5457b
Eduardo Popovici <32421250+edupopov@users.noreply.github.com>
2025-09-09 21:27:54 -0300

    Configure and validate time server settings

--- ServidorDeHorario.ps1
+++ ServidorDeHorario.ps1
@@ -3,6 +3,13 @@
   Criado por Eduardo Popovici
 #>
 
+# Configurar o servidor de horário e forçar a replicação
 w32tm /config /manualpeerlist:"pool.ntp.org" /syncfromflags:manual /reliable:yes /update
 net stop w32time
 net start w32time
+w32tm /resync /rediscover
+
+# Validar Funcionamento 
+w32tm /query /status
+w32tm /query /configuration
+w32tm /query /peers
